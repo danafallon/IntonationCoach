cc429406852ced4ff9781f672783b4fbebabd6d8
Dana Fallon <danajfallon@gmail.com>
2015-05-19 14:20:56 -0700

    recorder working; recording being passed to server successfully but issue in analyzing it

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 """Server for Intonation Coach."""
 
-from flask import Flask, render_template, session, send_from_directory
+from flask import Flask, render_template, session, send_from_directory, request
 from flask_debugtoolbar import DebugToolbarExtension
 import jinja2
 import json
+import base64
 
 import model
 from pitchgraph import praat_analyze_pitch, format_pitch_data
@@ -38,22 +39,32 @@ def french_content():
 	return render_template("french.html")
 
 
-@app.route('/sounds/<path:filename>')
-def send_audio_file(filename):
-    return send_from_directory('static/sounds', filename)
+@app.route('/<path:path>')
+def send_audio_file(path):
+	"""Given a url of an audio file, return that file as stored in static folder."""
 
-@app.route('/analyze')
+	return send_from_directory('static', path)
+
+
+@app.route('/analyze', methods=["POST"])
 def analyze_user_rec():
 	"""Analyze the user's recording and send pitch data for both recordings back to page."""
 
 	# fetch target recording's pitch data:
 	target_filepath = request.form.get("sentence") + "_pd.json"
+	print target_filepath
 	target_file = open(target_filepath)
 	target_pitch_data = json.loads(target_file.read())
 
 	# analyze user's recording:
-	user_audio = request.form.get("user_file")
-	user_pitch_data = format_pitch_data(praat_analyze_pitch(user_audio))
+	user_b64 = request.form.get("user_rec")[22:]
+	print len(user_b64)
+	user_wav = base64.b64decode(user_b64)
+	f = open('user-rec.wav', 'wb')
+	f.write(user_wav)
+	f.close()
+
+	user_pitch_data = format_pitch_data(praat_analyze_pitch('user-rec.wav'))
 	return [target_pitch_data, user_pitch_data]
 
 
--- static/js/recorder.js
+++ static/js/recorder.js
@@ -1,6 +1,6 @@
 (function(window){
 
-  var WORKER_PATH = 'recorderWorker.js';
+  var WORKER_PATH = 'static/js/recorderWorker.js';
 
   var Recorder = function(source, cfg){
     var config = cfg || {};
