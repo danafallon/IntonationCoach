e9724ed3f4622cc57663f86a74282d4a4b22d18e
Dana Fallon <danajfallon@gmail.com>
2017-06-04 17:53:08 -0700

    Lint tests

--- test/test_pitchgraph.py
+++ test/test_pitchgraph.py
@@ -1,63 +1,53 @@
+import json
+import os
+import sys
 import unittest
 
-import sys
-from os import path
-sys.path.append(path.dirname(path.dirname( path.abspath(__file__) )))
-
-from pitchgraph import get_praat_pitch, format_pitch_data, smooth_pitch_data, analyze_pitch
-import json
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from pitchgraph import get_praat_pitch, format_pitch_data, smooth_pitch_data
 
 
 class PitchgraphTestCase(unittest.TestCase):
-	"""Tests for 'pitchgraph.py'."""
+    """Tests for pitchgraph.py"""
 
-	def setUp(self):
-		self.audio_file = path.abspath('static/sounds/en-us-1.wav')
+    def setUp(self):
+        self.audio_file = os.path.abspath('static/sounds/en-us-1.wav')
 
-	def test_analyze_pitch_returns_dict(self):
-		"""Does get_praat_pitch return a dict of the format expected by format_pitch_data?"""
-		
-		pitch_data = get_praat_pitch(self.audio_file)
-		
-		self.assertIsInstance(pitch_data, dict)
-		
-		akey = pitch_data.keys()[0]
-		value_dict = pitch_data[akey]
-		
-		self.assertIsInstance(akey, float)
-		self.assertIsInstance(value_dict, dict)
-		self.assertIn("Pitch", value_dict)
+    def test_get_praat_pitch_format(self):
+        """Does get_praat_pitch return a dict of the format expected by format_pitch_data?"""
 
-	def test_pitch_data_formatting(self):
-		"""Does format_pitch_data return a list of dicts that contain "x" and "y" as keys?"""
+        pitch_data = get_praat_pitch(self.audio_file)
+        self.assertIsInstance(pitch_data, dict)
 
-		formatted_pitch_data = format_pitch_data(get_praat_pitch(self.audio_file))
-		
-		self.assertIsInstance(formatted_pitch_data, list)
-		self.assertIsInstance(formatted_pitch_data[0], dict)
-		self.assertIn("x", formatted_pitch_data[0])
-		self.assertIn("y", formatted_pitch_data[0])
+        akey = pitch_data.iterkeys().next()
+        value_dict = pitch_data[akey]
+        self.assertIsInstance(akey, float)
+        self.assertIsInstance(value_dict, dict)
+        self.assertIn("Pitch", value_dict)
 
-	def test_smooth_pitch_data(self):
-		"""Does smooth_pitch_data reduce the number of datapoints?"""
+    def test_format_pitch_data(self):
+        """Does format_pitch_data return a list of dicts that contain "x" and "y" as keys?"""
 
-		unsmoothed = format_pitch_data(get_praat_pitch(self.audio_file))
-		smoothed = json.loads(smooth_pitch_data(unsmoothed))
+        formatted_pitch_data = format_pitch_data(get_praat_pitch(self.audio_file))
+        self.assertIsInstance(formatted_pitch_data, list)
+        self.assertIsInstance(formatted_pitch_data[0], dict)
+        self.assertIn("x", formatted_pitch_data[0])
+        self.assertIn("y", formatted_pitch_data[0])
 
-		self.assertLess(len(smoothed), len(unsmoothed))
+    def test_smooth_pitch_data(self):
+        """Does smooth_pitch_data reduce the number of datapoints?"""
 
-	def test_empty_if_no_audio_captured(self):
-		"""Does smooth_pitch_data return an empty list (in a JSON string) if no audio was captured?"""
+        unsmoothed = format_pitch_data(get_praat_pitch(self.audio_file))
+        smoothed = json.loads(smooth_pitch_data(unsmoothed))
+        self.assertLess(len(smoothed), len(unsmoothed))
 
-		unsmoothed = []
-		smoothed = json.loads(smooth_pitch_data(unsmoothed))
+    def test_empty_if_no_audio_captured(self):
+        """Does smooth_pitch_data return an empty list (in a JSON string)
+        if no audio was captured?"""
 
-		self.assertEqual(smoothed, [])
+        smoothed = json.loads(smooth_pitch_data([]))
+        self.assertEqual(smoothed, [])
 
 
 if __name__ == "__main__":
-	unittest.main()
-
-
-
-
+    unittest.main()
--- test/test_server.py
+++ test/test_server.py
@@ -1,61 +1,53 @@
+import os
+import sys
+import tempfile
 import unittest
 
-import sys, os
-from os import path
-sys.path.append(path.dirname(path.dirname( path.abspath(__file__) )))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import server
-
-import tempfile
 
 
 class ServerTestCase(unittest.TestCase):
-	"""Tests for 'server.py'."""
+    """Tests for server.py"""
 
-	def setUp(self):
-		self.db_fd, server.app.config['DATABASE'] = tempfile.mkstemp()
-		server.app.config['TESTING'] = True
-		self.app = server.app.test_client()
+    def setUp(self):
+        self.db_fd, server.app.config['DATABASE'] = tempfile.mkstemp()
+        server.app.config['TESTING'] = True
+        self.app = server.app.test_client()
 
-	def tearDown(self):
-		os.close(self.db_fd)
-		os.unlink(server.app.config['DATABASE'])
+    def tearDown(self):
+        os.close(self.db_fd)
+        os.unlink(server.app.config['DATABASE'])
 
-	def test_renders_english_template(self):
-		"""Does the English page load from the expected route?"""
-		
-		res = self.app.get('/english-us')
-		self.assertIn('English (US)', res.data)
-		self.assertIn('Overview', res.data)
-		self.assertIn('Declarative', res.data)
+    def test_renders_english_template(self):
+        """Does the English page load from the expected route?"""
 
-	def test_renders_french_template(self):
-		"""Does the French page load at the expected route?"""
-		
-		res = self.app.get('/french')
-		self.assertIn('French', res.data)
-		self.assertIn('Overview', res.data)
-		self.assertIn('Declarative', res.data)
+        res = self.app.get('/english-us')
+        self.assertIn('English (US)', res.data)
+        self.assertIn('Overview', res.data)
+        self.assertIn('Declarative', res.data)
 
-	def test_send_audio_file(self):
-		"""Is an audio file successfully fetched from the static folder?"""
-		
-		res = self.app.get('/sounds/en-us-1.wav')
-		self.assertIs(res.status_code, 200)
+    def test_renders_french_template(self):
+        """Does the French page load at the expected route?"""
 
-	@unittest.skip('')
-	def test_send_target_pitch_data(self):
-		"""Is target pitch data being sent to the page?"""
-		
-		res = self.app.post('/targetdata', data=dict(sentence='en-us-1'))		
-		self.assertIsNotNone(res.data)
-		
+        res = self.app.get('/french')
+        self.assertIn('French', res.data)
+        self.assertIn('Overview', res.data)
+        self.assertIn('Declarative', res.data)
+
+    def test_send_audio_file(self):
+        """Is an audio file successfully fetched from the static folder?"""
+
+        res = self.app.get('/sounds/en-us-1.wav')
+        self.assertIs(res.status_code, 200)
+
+    @unittest.skip('')
+    def test_send_target_pitch_data(self):
+        """Is target pitch data being sent to the page?"""
+
+        res = self.app.post('/targetdata', data=dict(sentence='en-us-1'))
+        self.assertIsNotNone(res.data)
+
 
 if __name__ == "__main__":
-	unittest.main()
-
-
-
-
-
-
-
+    unittest.main()
